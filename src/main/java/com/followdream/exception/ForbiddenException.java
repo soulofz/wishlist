e14843e92c6a551d93f1b4bba43965843aeadcde
soulofz <zhanminskzhan@gmail.com>
2025-12-17 19:50:51 +0300

    Exceptions

---
+++ src/main/java/com/followdream/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package com.followdream.exception;
+
+public class ForbiddenException extends Exception{
+    @Override
+    public String toString(){
+        return "ForbiddenException";
+    }
+}
---
+++ src/main/java/com/followdream/exception/UserNotFoundException.java
@@ -0,0 +1,24 @@
+package com.followdream.exception;
+
+import by.tms.model.User;
+
+public class UserNotFoundException extends RuntimeException {
+    private final User user;
+
+    public UserNotFoundException(User user) {
+        super();
+        this.user = user;
+    }
+
+    public UserNotFoundException(int id) {
+        super();
+        User user = new User();
+        user.setId(id);
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "UserNotFoundException{user with id = " + user.getId() + " not found}";
+    }
+}
