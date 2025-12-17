a9ccb4bcef199a44c250e1e49a6e9f49bedd8d7d
soulofz <zhanminskzhan@gmail.com>
2025-12-17 22:30:17 +0300

    DTO created

---
+++ src/main/java/com/followdream/model/dto/UserRegistrationDto.java
@@ -0,0 +1,57 @@
+package com.followdream.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRegistrationDto {
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = ".*\\d.*", message = "Password must contain at least one digit")
+    private String password;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
