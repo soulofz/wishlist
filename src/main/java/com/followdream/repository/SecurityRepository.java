cf5a9b966df9b214028826d5ae5be04a2d0ce356
soulofz <zhanminskzhan@gmail.com>
2025-12-17 15:48:38 +0300

    Repositories added

---
+++ src/main/java/com/followdream/repository/SecurityRepository.java
@@ -0,0 +1,7 @@
+package com.followdream.repository;
+
+import com.followdream.model.Security;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecurityRepository extends JpaRepository<Security, Long> {
+}
