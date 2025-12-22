88926d609ba38b0454524275a2b83b520f229b92
soulofz <zhanminskzhan@gmail.com>
2025-12-22 15:37:14 +0300

    User Response Dto added

--- src/main/java/com/followdream/model/dto/UserResponseDto.java
+++ src/main/java/com/followdream/model/dto/UserResponseDto.java
@@ -1,4 +1,21 @@
 package com.followdream.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDto {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private LocalDate birthday;
+    private File avatar;
 }
