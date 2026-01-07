18ed3adc1e077c89e1c64a84e38a19f173a510cf
soulofz <zhanminskzhan@gmail.com>
2026-01-07 14:41:05 +0300

    WishlistPolicyService.java added

---
+++ src/main/java/com/wishlist/service/WishlistPolicyService.java
@@ -0,0 +1,74 @@
+package com.wishlist.service;
+
+import com.wishlist.model.User;
+import com.wishlist.model.Wishlist;
+import com.wishlist.model.enums.CompletedGiftPolicy;
+import com.wishlist.model.enums.ReservationVisibilityPolicy;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishlistPolicyService {
+
+    private final FriendService friendService;
+
+    public WishlistPolicyService(FriendService friendService) {
+        this.friendService = friendService;
+    }
+
+    public boolean isVisibleForUser(Wishlist wishlist, User requester) {
+
+        if (wishlist.getOwner().equals(requester)) {
+            return true;
+        }
+
+        switch (wishlist.getVisibilityPolicy()) {
+            case PUBLIC -> {
+                return true;
+            }
+            case FRIENDS_ONLY -> {
+                return friendService.areFriends(wishlist.getOwner(), requester);
+            }
+            case PRIVATE -> {
+                return false;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    public boolean canReserveItems(Wishlist wishlist, User requester) {
+        if (requester == null) {
+            return false;
+        }
+        if (wishlist.getOwner().equals(requester)) {
+            return false;
+        }
+        switch (wishlist.getReservationPolicy()) {
+            case PUBLIC -> {
+                return true;
+            }
+            case FRIENDS_ONLY -> {
+                return friendService.areFriends(wishlist.getOwner(), requester);
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    public boolean isOwnerSeeReservationDetails(Wishlist wishlist) {
+        return wishlist.getReservationVisibilityPolicy()
+                == ReservationVisibilityPolicy.FULL_VISIBLE;
+    }
+
+    public boolean isReservationVisibleToOwner(Wishlist wishlist) {
+        return wishlist.getReservationVisibilityPolicy()
+                != ReservationVisibilityPolicy.HIDDEN;
+    }
+
+    public boolean shouldRemoveCompletedGift(Wishlist wishlist) {
+        return wishlist.getCompletedGiftPolicy()
+                == CompletedGiftPolicy.REMOVE;
+    }
+}
