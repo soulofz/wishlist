3b65c0e4b5da873d7e1b31b11fc1b63c47641c0d
soulofz <zhanminskzhan@gmail.com>
2026-01-07 14:39:55 +0300

    WishlistRepository.java edited

--- src/main/java/com/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/wishlist/repository/WishlistRepository.java
@@ -1,7 +1,12 @@
 package com.wishlist.repository;
 
+import com.wishlist.model.User;
 import com.wishlist.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+    List<Wishlist> findAllByOwner(User owner);
 }
