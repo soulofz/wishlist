bc7896718533646a31fd83569f6940bbfe61f768
soulofz <zhanminskzhan@gmail.com>
2026-01-09 20:31:01 +0300

    CloudImageService API

---
+++ src/main/java/com/wishlist/service/CloudImageService.java
@@ -0,0 +1,44 @@
+package com.wishlist.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudImageService {
+
+    private final Cloudinary cloudinary;
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    public CloudImageService() {
+
+        cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
+
+    public String uploadImage(MultipartFile file, String folder) throws IOException {
+
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(),
+                ObjectUtils.asMap("folder", folder));
+
+        String imageUrl = (String) uploadResult.get("secure_url");
+        return imageUrl; //
+    }
+}
+
